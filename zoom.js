5dd41191ca0aa69fabc5fa3203d630186ca95d68
heliogabal <rhalbmann@gmail.com>
2015-03-10 20:56:32 +0100

    first

---
+++ zoom.js
@@ -0,0 +1,15 @@
+(function ($, Drupal, window, document, undefined) {
+(function ($) {
+  Drupal.behaviors.hk_leaflet = {
+    attach: function (context, settings) {
+      $(document).bind('leaflet.feature', function(e, lFeature, feature) {
+        lFeature.on('click', function(e) {
+          var lMap = Drupal.settings.leaflet[0].lMap;
+          lMap.setView(e.latlng,16,.5,.25,true);
+        })
+      });
+
+    }
+  }
+})(jQuery);
+})(jQuery, Drupal, this, this.document);
