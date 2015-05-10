44e64482cf25d5ebc9a0707a907a29c891dfdbca
heliogabal <rhalbmann@gmail.com>
2015-05-11 01:49:12 +0200

    popup more button

--- zoom.js
+++ zoom.js
@@ -1,23 +1,27 @@
 (function ($, Drupal, window, document, undefined) {
-(function ($) {
-  Drupal.behaviors.hk_leaflet = {
-    attach: function (context, settings) {
-      $(document).bind('leaflet.feature', function(e, lFeature, feature) {
-        lFeature.on('click', function(e) {
-          var lMap = Drupal.settings.leaflet[0].lMap;
-          lMap.setView(e.latlng,18,1,.15,true);
-          $('.apart').slice(3).css("display","none");
-        var toggleMinus = 'sites/all/themes/hk_theme/images/bullet_toggle_minus.png';
-        var togglePlus = 'sites/all/themes/hk_theme/images/bullet_toggle_plus.png';
-        var show = Drupal.t("Show All");
-        var hide = Drupal.t("Hide");
-        })
-      });
-/*      $(document).bind('submit','#edit-submit-karten', function(e) {
-          var lMap = Drupal.settings.leaflet[0].lMap;
-          lMap.fitBounds(e.latlng,16,.5,.25,true);
-              });*/
+   (function ($) {
+    Drupal.behaviors.hk_leaflet = {
+      attach: function (context, settings) {
+        $(document).bind('leaflet.feature', function(e, lFeature, feature) {
+          lFeature.on('click', function(e) {
+            var lMap = Drupal.settings.leaflet[0].lMap;
+            lMap.setView(e.latlng,18,1,.15,true);
+            $('.leaflet-popup-content').find('.apart:gt(2)').css("display","none").addClass('toggle');
+            var toggleMinus = 'sites/all/themes/hk_theme/images/bullet_toggle_minus.png';
+            var togglePlus = 'sites/all/themes/hk_theme/images/bullet_toggle_plus.png';
+            var show = Drupal.t("Alle zeigen");
+            var hide = Drupal.t("Verbergen");
+            $('.tablehead .more').replaceWith('<button class="toggle" data-text-original="Alle zeigen" data-text-swap="Verbergen"><strong class="show">' + show + '</strong></button>');
+            $('button.toggle').click(function() {
+              $('.apart.toggle').toggle();
+              var el = $(this);
+              el.text() == el.data("text-swap")
+                ? el.text(el.data("text-original"))
+                : el.text(el.data("text-swap"));
+            })
+          });
+        });
+      }
     }
-  }
-})(jQuery);
+   })(jQuery);
 })(jQuery, Drupal, this, this.document);
