1e21bce4795005b0260732cc04a74cc99e252684
heliogabal <rhalbmann@gmail.com>
2015-08-14 00:38:22 +0200

    refactor

--- hk_leaflet_script.js
+++ hk_leaflet_script.js
@@ -7,8 +7,8 @@
             var lMap = Drupal.settings.leaflet[0].lMap;
             lMap.setView(e.latlng,18,1,.15,true);
             var n = $(".leaflet-popup-content .apart").length;
-            if ( n > 3) {
-                        $('.leaflet-popup-content').find('.apart:gt(2)').css("display","none").addClass('toggle');
+            if ( n > 4) {
+                        $('.leaflet-popup-content').find('.apart:gt(3)').css("display","none").addClass('toggle');
                         var toggleMinus = 'sites/all/themes/hk_theme/images/bullet_toggle_minus.png';
                         var togglePlus = 'sites/all/themes/hk_theme/images/bullet_toggle_plus.png';
                         var show = Drupal.t("Show all");
@@ -24,19 +24,21 @@
                       }
           });
         });
+      $( ".front #block-views-exp-karten-map-page, .front #search").dialog({
+            height: "auto",
+            minHeight: "120",
+            width: "auto",
+            resizable: "false",
+            closeOnEscape: "true",
+            hide: { effect: "explode", duration: "1000" },
+            //hide: "false",
+            position: { my: "center bottom", at: "center-180 bottom-80", collision: "fit flip", of: "#page" }
+          });
+      $( '#block-menu-menu-service-menu .menu li:nth-child(2)').click(function(e) {
+          $( ".ui-dialog,#block-views-exp-karten-kaufen,#block-views-exp-karten-mieten").toggle();
+            e.preventDefault();
+          });
       }
     }
    })(jQuery);
 })(jQuery, Drupal, this, this.document);
-
-
-      //     /* Transform not working on PDF creation*/
-      // var transform=$(".page-node-expose .leaflet-map-pane").find(".leaflet-tile-container>img").css("transform");
-      // var comp=transform.split(",");
-      // var mapleft=parseFloat(comp[4]);
-      // var maptop=parseFloat(comp[5]);
-      // $(".page-node-expose .leaflet-map-pane").find(".leaflet-tile-container>img").css({
-      //     "transform":"none",
-      //     "left":mapleft,
-      //     "top":maptop,
-      // });
